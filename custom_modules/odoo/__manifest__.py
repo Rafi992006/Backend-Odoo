cbe3d264af7972396d877b626fcca1be48b0cdd6
Rafi992006 <rafimnaufal259@gmail.com>
2024-07-05 16:32:27 +0700

    Add files via upload

---
+++ custom_modules/odoo/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Biodata',
+    'version': '0.1',
+    'data': [
+        "security/ir.model.access.csv",
+        "views/biodata.views.xml",
+        "views/menuitems.views.xml",
+            
+    ],
+    'installable': True
+
+
+
+}
